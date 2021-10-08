42f37ed80f99262d7064bfadfde915aa118bed61
Master-Sniffer <dsolodennikov@gmail.com>
2021-10-08 20:42:01 +0300

    Обработал немного дизайн, обновил структуру

--- VTB_APP/app/src/main/java/com/example/vtb_app/MainActivity.java
+++ VTB_APP/app/src/main/java/com/example/vtb_app/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.vtb_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText EnterText;
+    private Button button_acpt;
+    private TextView View_Result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EnterText = findViewById(R.id.EnterText);
+        button_acpt = findViewById(R.id.button_acpt);
+        View_Result = findViewById(R.id.View_Result);
+
+        button_acpt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (EnterText.getText().toString().trim().equals("")){
+                    Toast.makeText(MainActivity.this, R.string.No_User_Input, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
