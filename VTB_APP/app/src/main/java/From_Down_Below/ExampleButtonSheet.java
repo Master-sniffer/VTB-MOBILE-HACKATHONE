9c26e9fcbacf238aa227aa83a89e385b7abeb1ae
Master-Sniffer <dsolodennikov@gmail.com>
2021-10-09 21:12:30 +0300

    Добавил функционал высплывания снизу окна

--- VTB_APP/app/src/main/java/From_Down_Below/ExampleButtonSheet.java
+++ VTB_APP/app/src/main/java/From_Down_Below/ExampleButtonSheet.java
@@ -1,9 +1,11 @@
 package From_Down_Below;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +15,47 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class ExampleButtonSheet extends BottomSheetDialogFragment {
 
+    private BottomSheetListener mListener;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.bottom_sheet_layout, container, false);
+
+        Button button1 = v.findViewById(R.id.button_popp1);
+        Button button2 = v.findViewById(R.id.button_popp2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onButtonClicked("Button 1 clicked");
+                dismiss();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onButtonClicked("Button 2 clicked");
+                dismiss();
+            }
+        });
+
         return v;
+    }
+
+    public interface BottomSheetListener {
+        void onButtonClicked(String text);
+    }
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+
+        try {
+            mListener = (BottomSheetListener) context;
+        }catch (Exception e){
+            throw new ClassCastException(context.toString()+ " must implement BottomSheetListener");
+        }
     }
 }
--- VTB_APP/app/src/main/java/com/example/vtb_app/MainPaige.java
+++ VTB_APP/app/src/main/java/com/example/vtb_app/MainPaige.java
@@ -3,10 +3,15 @@ package com.example.vtb_app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
-public class MainPaige extends Activity {
+import From_Down_Below.ExampleButtonSheet;
+
+public class MainPaige extends AppCompatActivity implements ExampleButtonSheet.BottomSheetListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,15 +19,22 @@ public class MainPaige extends Activity {
         setContentView(R.layout.word_item_view);
 
 
+
     }
 
     public void Vse_Ravno(View view){
 
-        
+
     }
 
     public void Stat_Clientom(View view){
+        ExampleButtonSheet bottom_sheet = new ExampleButtonSheet();
+        bottom_sheet.show(getSupportFragmentManager(), "example bottom sheet");
 
     }
 
+    @Override
+    public void onButtonClicked(String text) {
+        System.out.println(text);
+    }
 }
